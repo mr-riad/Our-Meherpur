486508175821526daf8b340083d92ec1e2518efa
mr-riad <riadgangni01302@gmail.com>
2025-06-19 23:19:03 +0600

    body slider add

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:our_meherpur/pages/home/appbar.dart';
+import 'package:our_meherpur/widgets/drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,7 +12,29 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CAppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Our Meherpur"),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))],
+      ),
+      drawer: DrawerWidget(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzT4hx8Rn9t_iAelfZCG8cosdaCD5iOGQjwQ&s",
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
+
+
